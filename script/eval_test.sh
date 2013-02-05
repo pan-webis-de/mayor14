409c98378f3961c8853a28d62764b875fc8c2116
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2013-02-04 22:48:56 -0600

    Removing tempral eval_test file

--- script/eval_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Positive evaluation"
-python src/authorid.py data/palacio_valdes/ data/palacio_valdes_pos.txt
-
-echo "Negative evaluation"
-python src/authorid.py data/palacio_valdes/ data/palacio_valdes_neg.txt
- 
