a603080093918c25b1f2a870413d293ee70f473e
Ivan Vladimir <ivanvladimir@gmail.com>
2013-02-18 21:02:58 -0600

    Fixes some documentation

--- README.md
+++ README.md
@@ -10,4 +10,16 @@ Requirements
 * python 2.7
 * scikit-learn
 
+Optional:
 
+* PAN dataset
+
+
+Documentation
+-------------
+
+Create the documentation with makefile
+
+::
+  
+   make doc
--- script/train_pan13.sh
+++ script/train_pan13.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Running training pan"
-python src/authorid.py  -m train -v data/PAN13-AuthorIdentification-TrainingCorpus data/PAN13-AuthorIdentification-TrainingCorpus/Answers.txt
+python src/authorid.py  $* -m train -v data/PAN13-AuthorIdentification-TrainingCorpus data/PAN13-AuthorIdentification-TrainingCorpus/Answers.txt
