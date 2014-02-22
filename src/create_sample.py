7a62896815d7b0729fa3a3ad153813e61cc70ba3
Josue Gutierrez <josue.ggh@gmail.com>
2014-02-22 10:34:43 -0600

    sample (add): comments

--- src/create_sample.py
+++ src/create_sample.py
@@ -1,23 +1,84 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 # 
+# Example of usage : 
+# The main function to get all the documents of a directory the next structure 
+# {'DIRNAME' : {'total' : {WORD COUNTS} , 'FILENAME' : {WORD COUNTS}} }
+#
+# {'EN04' : {'total: {'worda':4,'wordb':2,'wordc':3}, 'know01.txt': {'worda':2} ,'unknow.txt' : {'wordc':1} } }
+#
+
 import random,glob, sys,os, getopt
 import docread as dr
 import numpy as np
 
+
+#
+# Variable
+# --------
+# lang
+# Dictionary with the percentage of search for every language
+# 
+# Skeleton
+# --------
+# percent (float):
+#	Percent of search
+#
 lang = {
 	'SP': {'percent': 0.40},
-	'EN': {'percent': 0.50},
+	'EN': {'percent': 0.01},
 	'GR': {'percent': 0.30},
+	'DE': {'percent': 0.80},
+	'DR': {'percent': 0.70},
+	'EE': {'percent': 0.30},
 }
 
-def langPercent(name):
-	return lang[name[:2]]['percent']
+#
+# Function
+# --------
+# langPercent
+# Function to get the language of a directory and get the percent of the language
+#
+# Parameters
+# ----------
+# directory (string):
+#	Name of the directory , example : ES001 , EN001
+def langPercent(directory):
+	return lang[directory[:2]]['percent']
 
-
+#
+# Function
+# -------
+# getSelection 
+# Function to get the same elements of two different list
+#
+# Parameters 
+# ----------
+# 
+# counter (list)
+#	Count list example {casa : 1 , verde : 3}
+# selection (list)
+#	Sample of list to be selected
+#
 def getSelection(counter, selection):
 	return {element : counter[element] for element in selection }
 
+#
+# Function
+# --------
+# getSample
+# Function to get the sample of all the files of directory
+# 	First we read all the documents of a directory and are concatenated at one Var : docs
+# 	With var "docs" we get the BOW reponse (include in docread.py)
+#	BOW response get the count of the words, we get a sample in relation of the percetage of a language , we call this process 'total'
+#	With our total, we process every single file and get the same variables as total.
+#	The final response is a single list. Example : {'EN04' : {'total: {'worda':4,'wordb':2,'wordc':3}, 'know01.txt': {'worda':2} ,'unknow.txt' : {'wordc':1} } } 
+#
+# Parameters
+# ----------
+# path(string)
+# Path of the files
+#
 def getSample(path):
 	problems = dr.problems(dr.dirproblems(path,r".*\.txt"))
 	
@@ -30,17 +91,22 @@ def getSample(path):
 			docs = docs + file[1]
 
 		count = dr.bow(docs)	
-		
+	
+		#Sample : Number of words to be obtained based on a percentage of the language	
 		sample = int(round(float(len(count[0]))*float(percent)))  
+		#Sample of words of all the total of docs
 		selection= random.sample(count[0],sample)
 	
+		#We select only the selection in the "count list"
 		data[dirname]['total'] = getSelection(count[0],selection)
 
 		for file in files:
 			namefile = file[0].split("/")[-1]
 			count_file = dr.bow(file[1])
+			#We geet the same selection as the final, for every single count of the file
 			data[dirname][namefile] = getSelection(count_file[0],selection)
-		
+	
+	print data	
 	return  data
 	
 def main(argv):
