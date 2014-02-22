33d97feee855cac30ce664b0a0a0576da14a0f5c
Josue Gutierrez <josue.ggh@gmail.com>
2014-02-21 22:59:55 -0600

    Create Sample(add): Returning the counters in the right format

--- src/create_sample.py
+++ src/create_sample.py
@@ -7,29 +7,41 @@ import numpy as np
 
 lang = {
 	'SP': {'percent': 0.40},
-	'EN': {'percent': 0.35},
+	'EN': {'percent': 0.50},
 	'GR': {'percent': 0.30},
 }
-
 
 def langPercent(name):
 	return lang[name[:2]]['percent']
 
+
+def getSelection(counter, selection):
+	return {element : counter[element] for element in selection }
+
 def getSample(path):
-	print path
 	problems = dr.problems(dr.dirproblems(path,r".*\.txt"))
-
+	
+	data = {}
 	for dirname, (files,unknow) in problems:
-
+		data[dirname]={}
 		percent = langPercent(dirname)
-		print percent
 		docs = ""
 		for file in files:
 			docs = docs + file[1]
-		count = dr.bow(docs)	
-		tup = [x for x in  sorted(count[0].iteritems() , key=lambda tup:tup[1],reverse = True)]
 
-		sample = int(round(float(len(tup))*float(percent)))
+		count = dr.bow(docs)	
+		
+		sample = int(round(float(len(count[0]))*float(percent)))  
+		selection= random.sample(count[0],sample)
+	
+		data[dirname]['total'] = getSelection(count[0],selection)
+
+		for file in files:
+			namefile = file[0].split("/")[-1]
+			count_file = dr.bow(file[1])
+			data[dirname][namefile] = getSelection(count_file[0],selection)
+		
+	return  data
 	
 def main(argv):
 	seed = ""
@@ -44,8 +56,8 @@ def main(argv):
 
 	try:
 		getSample(seed)
-	except:
-		print "Bad parameters"
+	except ValueError:
+		print ValueError
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
