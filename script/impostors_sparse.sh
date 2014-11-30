21f1ed049365dfedcd7a2a78a05b5d0367669c1b
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2014-11-30 14:19:44 -0600

    Fixes problem with impostors experiment script

--- script/impostors_sparse.sh
+++ script/impostors_sparse.sh
@@ -3,10 +3,10 @@
 diez=10
 for IMP in 1 2 3 4 5 6 7 8
 do
-	python src/authorid_sparse.py --nimpostors ${IMP} -m devel --impostors data/imposters/EN --dumpfiles -r capital -r numbers -r punct -r bow -r lemma -r pos -r nstcs data/pan14_train/pan14-author-verification-training-corpus-english-novels-2014-04-22 data/pan14_train/pan14-author-verification-training-corpus-english-novels-2014-04-22/truth.txt > data/pan14_train/pan14-author-verification-training-corpus-english-novels-2014-04-22/answers_all.txt
-	dir="final/en_2i_punct_nstcs_imposotors_${IMP}"
+	python src/authorid_sparse.py --nimpostors ${IMP} -m devel --impostors data/imposters/EN --dumpfiles -r capital -r numbers -r punct -r bow -r lemma -r pos -r nstcs data/pan14_train/pan14-author-verification-training-corpus-spanish-articles-2014-04-22 data/pan14_train/pan14-author-verification-training-corpus-spanish-articles-2014-04-22/truth.txt > data/pan14_train/pan14-author-verification-training-corpus-spanish-articles-2014-04-22/answers_all.txt
+	dir="final/sp_capital_numbers_punct_bow_lema_pos_nstcs_impostors_${IMP}"
 	echo $dir
 	mkdir $dir
 	cp answers*.dump $dir
-	cp data/pan14_train/pan14-author-verification-training-corpus-english-novels-2014-04-22/answers_all.txt $dir
+	cp data/pan14_train/pan14-author-verification-training-corpus-spanish-articles-2014-04-22/answers_all.txt $dir
 done
